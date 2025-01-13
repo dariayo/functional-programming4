9de3bb8ee3e72fd9c07b9fc364e94769c103f06f
Daria Shevchenko <daria.pinkfloyd@yandex.ru>
2025-01-13 19:09:52 +0300

    delete unused files

--- lab4/scripts/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Starting Docker Compose services..."
-docker-compose up -d
-
-echo "Application started. Access it at http://localhost:8080"
--- lab4/scripts/stop.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Stopping Docker Compose services..."
-docker-compose down
-
-echo "Application stopped."
